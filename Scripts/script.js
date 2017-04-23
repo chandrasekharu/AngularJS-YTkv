509cc99e267fd75ad229e7b510735c0a0fc2999f
chandrasekharu <ucsraju@gmail.com>
2017-04-23 15:33:31 -0500

    filters format, limit

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,22 +3,15 @@
 var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
-        var technologies = [
-            { name: "c#", likes: 0, dislikes: 0 },
-            { name: "Java", likes: 0, dislikes: 0 },
-            { name: "ASP.net", likes: 0, dislikes: 0 },
-            { name: "AngularJS", likes: 0, dislikes: 0 },
-
+        var employees = [
+            { name: "RamaChandra", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
+            { name: "Jai Kumar", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
+            { name: "Chandra", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
+            { name: "Karthik", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
+            { name: "Sama", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
         ];
-        $scope.technologies = technologies;
-        $scope.incrementLikes = function (technology) {
-            technology.likes++;
-        }
-        $scope.incrementDislikes = function (technology) {
-            technology.dislikes++;
-        }
+        $scope.employees = employees;
+        $scope.rowLimit = 3;
 
-
-           
 
 });
