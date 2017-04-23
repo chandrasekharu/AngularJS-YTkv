6fc6ada20236783a14ae941754b504d63234f7d5
chandrasekharu <ucsraju@gmail.com>
2017-04-23 14:33:49 -0500

    event handler

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,19 +3,22 @@
 var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
-        var countries = [
-            {
-                name: "Rajasekhar",
-                cities: [
-                    { name: "Birmingham" },
-                    { name: "Birmingham" },
-                    { name: "Birmingham" }
-                ]
-            },
-            { name: "Rajasekhar", cities: [{ name: "Birmingham" }, { name: "Birmingham" }, { name: "Birmingham" }] },
-            { name: "Rajasekhar", cities: [{ name: "Birmingham" }, { name: "Birmingham" }, { name: "Birmingham" }] }
+        var technologies = [
+            { name: "c#", likes: 0, dislikes: 0 },
+            { name: "Java", likes: 0, dislikes: 0 },
+            { name: "ASP.net", likes: 0, dislikes: 0 },
+            { name: "AngularJS", likes: 0, dislikes: 0 },
+
         ];
-        $scope.countries = countries;   
+        $scope.technologies = technologies;
+        $scope.incrementLikes = function (technology) {
+            technology.likes++;
+        }
+        $scope.incrementDislikes = function (technology) {
+            technology.dislikes++;
+        }
+
+
            
 
 });
