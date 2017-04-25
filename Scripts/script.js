7b527d78998efdbd32d968139f1d7e260faf8aca
chandrasekharu <ucsraju@gmail.com>
2017-04-24 19:19:57 -0500

    sorting by orderBy

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,14 +4,14 @@ var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
         var employees = [
-            { name: "RamaChandra", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
-            { name: "Jai Kumar", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
-            { name: "Chandra", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
-            { name: "Karthik", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
-            { name: "Sama", dateOfBirth: new Date("November 23, 1980"), gender: "male", salary: 65000.78 },
+            { name: "RamaChandra", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.78 },
+            { name: "Jai Kumar", dateOfBirth: new Date("November 23, 1999"), gender: "Female", salary: 36000.78 },
+            { name: "Chandra", dateOfBirth: new Date("November 23, 2000"), gender: "Female", salary: 75000.78 },
+            { name: "Karthik", dateOfBirth: new Date("November 23, 2014"), gender: "male", salary: 89000.78 },
+            { name: "Sama", dateOfBirth: new Date("November 23, 1972"), gender: "Male", salary: 23000.78 },
         ];
         $scope.employees = employees;
-        $scope.rowLimit = 3;
+        $scope.sortColumn = "name";
 
 
 });
