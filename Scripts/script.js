c6fd8861b03d3feac350ee03cb1e1b9227f1cd56
chandrasekharu <ucsraju@gmail.com>
2017-04-26 00:52:27 -0500

    weather

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,15 +3,11 @@
 var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope) {
-        var employees = [
-            { name: "RamaChandra", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.78 },
-            { name: "Jai Kumar", dateOfBirth: new Date("November 23, 1999"), gender: "Female", salary: 36000.78 },
-            { name: "Chandra", dateOfBirth: new Date("November 23, 2000"), gender: "Female", salary: 75000.78 },
-            { name: "Karthik", dateOfBirth: new Date("November 23, 2014"), gender: "male", salary: 89000.78 },
-            { name: "Sama", dateOfBirth: new Date("November 23, 1972"), gender: "Male", salary: 23000.78 },
+        var products = [
+            { orderid:'12', name:"Mobile", price:"35", category:"electronics"  },
+            { orderid:'13', name: "Mobile", price: "35", category: "electronics" },
+            { orderid: '14', name: "Mobile", price: "35", category: "electronics" }
         ];
-        $scope.employees = employees;
-        $scope.sortColumn = "name";
-
-
+        $scope.products = products;
+        
 });
